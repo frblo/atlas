799e61320d1e7c7b45a6789dd288a3a3f8802a65
Fredrik Blomqvist <scanmar333@gmail.com>
2025-12-13 13:07:47 +0100

    upload and display of existing maps

---
+++ src/routes/maps/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let data;
+	export let form;
+</script>
+
+<h1>Map Manager</h1>
+
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="file" required />
+	<button type="submit">Upload Map</button>
+</form>
+
+{#if form?.success}
+	<p style="color: green;">Upload successful!</p>
+{/if}
+
+<hr />
+
+<h2>Available Maps</h2>
+
+{#if data.maps.length === 0}
+	<p>No maps uploaded yet.</p>
+{:else}
+	<ul>
+		{#each data.maps as mapName}
+			<a href="/maps/{mapName}">
+				<img src="../../../data/maps/{mapName}" width="10%" alt="Map" />
+				<p>{mapName}</p>
+			</a>
+		{/each}
+	</ul>
+{/if}
