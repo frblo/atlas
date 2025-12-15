286a171a04bc2c7370d271945caa6b4f7e98c7fd
Fredrik Blomqvist <scanmar333@gmail.com>
2025-12-15 20:33:37 +0100

    Favicon and basic navbar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,37 @@
+<nav>
+	<a href="/maps">Maps</a>
+	<a href="/">
+		<img src="/src/lib/assets/favicon.svg" height="50" alt="Logo" />
+	</a>
+	<a href="/markers">Markers</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		padding: 10px 0;
+		background-color: #333;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	img {
+		margin: 0 20px;
+		height: 50px;
+		width: auto;
+	}
+
+	a {
+		color: #fff;
+		text-decoration: none;
+		font-size: 1.1em;
+		padding: 5px 10px;
+		transition: color 0.2s;
+	}
+
+	a:hover {
+		color: #a0c4ff;
+	}
+</style>
